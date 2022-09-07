b03dc8929d2862d829b740f81ebd2d8755f654a5
Ankush Singh <ankushsahil45@gmail.com>
2022-09-07 23:45:42 +0530

    added movie.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Movie from "./pages/movie/Movie";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route index element={<Home />}></Route>
-          <Route path="movie/:id" element={<h1>Movie</h1>}></Route>
+          <Route path="movie/:id" element={<Movie />}></Route>
           <Route path="movie/:type" element={<h1>MovieList</h1>}></Route>
           <Route path="/*" element={<h1>Error Page</h1>}></Route>
         </Routes>
