1f423af2103cb1dd586a5e195a942a17e9e4c9fe
Ankush Singh <ankushsahil45@gmail.com>
2022-09-07 23:55:33 +0530

    added Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
 import Movie from "./pages/movie/Movie";
 import MovieList from "./components/movieList/MovieList";
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route index element={<Home />}></Route>
           <Route path="movie/:id" element={<Movie />}></Route>
